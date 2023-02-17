2d5775f393007289a30dcd013b8a6c7775fb8a7e
Mohamed Elhadouchi <melhadou@student.1337.ma>
2023-02-17 23:09:32 +0100

    print all lowercase alphabets using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all lowercase alphabets
+ * Return: 0 in case of success
+ */
+int main(void)
+{
+	int a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return 0;
+}
