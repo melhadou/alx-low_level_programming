c80d057f5a7973e9eb4659f09a55fbe115ccdd96
Mohamed Elhadouchi <melhadou@student.1337.ma>
2023-02-21 21:20:01 +0100

    print alphabets func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+	* print_alphabet - print alphabets
+*/
+void print_alphabet(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
