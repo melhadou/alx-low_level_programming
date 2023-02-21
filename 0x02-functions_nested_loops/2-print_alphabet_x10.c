3f486af9a7c3906af8319af868a55a6059905316
Mohamed Elhadouchi <melhadou@student.1337.ma>
2023-02-21 21:23:59 +0100

    print 10times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ *
+ * Return: 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int n, c;
+
+	c = 0;
+	while (c < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		c++;
+		_putchar('\n');
+	}
+}
