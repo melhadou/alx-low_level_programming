202c8fe43f3ffb0e677bb91bcedb794bec389608
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-02-21 12:23:56 +0100

    fix betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 /**
 	* main - print _putchar
-	* Retrun: 0 in success
+	* Retrun : 0 in success
 	*/
 int main(void)
 {
 	char *str = "_putchar\n";
 	int i = 0;
 
-	while(str[i])
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
